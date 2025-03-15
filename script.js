df1dee05fe0ebe97924a047cb746e1d431c9a3b2
tariel2010 <tariel.khabelashvili@gmail.com>
2025-03-15 12:08:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Выводим сообщение в консоль
+console.log("Сайт успешно загружен!");
+
+// Пример интерактивности: меняем цвет заголовка при клике
+document.querySelector("h1").addEventListener("click", function() {
+    this.style.color = "#e74c3c"; // Красный цвет
+});
